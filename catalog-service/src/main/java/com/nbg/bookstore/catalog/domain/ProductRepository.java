79961baf8f2950b521591cd29ef1ed786cefdcd7
khalidnbg <khalidnabgaoui4@gmail.com>
2025-12-17 00:31:36 +0100

    access denied for ./mvnw -ntp verify : make it executable

---
+++ catalog-service/src/main/java/com/nbg/bookstore/catalog/domain/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.nbg.bookstore.catalog.domain;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    Optional<ProductEntity> findByCode(String code);
+}
